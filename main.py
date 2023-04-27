3a33025f136deff93d62461f37a789be4a2f16e8
kagawa-moha <kagawamohamed329@gmail.com>
2023-04-27 16:30:13 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import psycopg2
+
+app = Flask(__name__)
+
+try:
+    conn = psycopg2.connect("dbname='myduka' user='posgres' host='localhost' password='12345678'")
+    print("Database Connected Successfully")
+except Exception as e:
+    print("I am unable to connect to the database",e)
+
+
+@app.route('/')
+def home():
+     return render_template('index.html')
+
+@app.route('/products')
+def products():
+    cur = conn.cursor()
+    cur.execute("""SELECT *  from products;""")
+    products = cur.fetchall()
+    return render_template('products.html', products=products)
+
+app.run()
